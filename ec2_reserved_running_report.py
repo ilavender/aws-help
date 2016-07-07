baa3c1f01413e3a7e3d574e0104c1299e268764f
Itamar Lavender <itamar.lavender@checkpoint.com>
2016-07-08 02:35:49 +0300

    report output ReservedAction and Budget

--- ec2_reserved_running_report.py
+++ ec2_reserved_running_report.py
@@ -105,9 +105,54 @@ def compare_reserved_runnin(MY_REGIONS):
     return MY_REPORT
 
 
+def find_offering(AZ, TYPE):
+    
+    REGION = AZ[:-1]
+    client = boto3.client('ec2', region_name=REGION)
+
+    response = client.describe_reserved_instances_offerings(
+        InstanceType=TYPE,
+        AvailabilityZone=AZ,
+        ProductDescription='Linux/UNIX (Amazon VPC)',        
+        InstanceTenancy='default',
+        OfferingType='Partial Upfront',
+        IncludeMarketplace=False,
+        MinDuration=31536000,
+        MaxDuration=31536000,
+        MaxResults=1
+    )
+    
+    for offering in response['ReservedInstancesOfferings']:
+        #print json.dumps(offering)                
+        return {'FixedPrice':offering['FixedPrice'], 'RecurringCharges':offering['RecurringCharges'], 'CurrencyCode':offering['CurrencyCode']}
+
 #print json.dumps(running_instances(MY_REGIONS))            
 #print json.dumps(active_reserved(MY_REGIONS))
-print json.dumps(compare_reserved_runnin(MY_REGIONS))
+#print json.dumps(compare_reserved_runnin(MY_REGIONS))
+#print json.dumps(find_offering('eu-west-1a', 'm4.xlarge'))
 
 
+MY_REPORT = compare_reserved_runnin(MY_REGIONS)
+WISH_LIST = []
+WISH_LIST_BUDGET = 0
 
+for I_AZ in MY_REPORT:
+    for I_TYPE in MY_REPORT[I_AZ]:
+        if MY_REPORT[I_AZ][I_TYPE] > 0:
+            offering = find_offering(I_AZ, I_TYPE)
+            upfront = (MY_REPORT[I_AZ][I_TYPE] * offering['FixedPrice'])
+            FixedPrice = offering['FixedPrice']
+            WISH_LIST_BUDGET = WISH_LIST_BUDGET + upfront
+            ACTION = 'Buy' 
+        elif MY_REPORT[I_AZ][I_TYPE] < 0:
+            FixedPrice = 0
+            ACTION = 'Sell'
+        elif MY_REPORT[I_AZ][I_TYPE] == 0:
+            FixedPrice = 0
+            ACTION = 'NA'
+            continue
+        
+        WISH_LIST.append({'AvailabilityZone':I_AZ, 'InstanceType':I_TYPE, 'Count':MY_REPORT[I_AZ][I_TYPE], 'FixedPrice':FixedPrice, 'Action':ACTION})
+
+
+print json.dumps({'ReservedAction':WISH_LIST, 'Budget':WISH_LIST_BUDGET})
